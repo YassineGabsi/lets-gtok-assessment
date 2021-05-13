a2885428d5041765ddadf54e7b3af7833d41f5d7
PsycoCast <gabsiyassine@gmail.com>
2021-05-13 22:54:35 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "./components/header/Header";
 import React from "react";
 import TopSection from "./components/top-section/TopSection";
+import Features from "./components/features-section/Features";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <TopSection/>
+      <Features/>
     </div>
   );
 }
---
+++ src/components/features-section/Features.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Features.css'
+
+function Features() {
+    return(
+        <div className="container text-center mt-5 ">
+            <h3>A community to spread <span style={{color: '#6930db', fontWeight: 'bold'}}>Positivity</span></h3>
+            <div className="col-12">
+                <div className="col-md-4">
+                    <div className="feature-card mb-5">
+                        aa
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Features;
+
