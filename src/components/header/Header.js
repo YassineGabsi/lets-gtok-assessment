e1b59c9731b833f2d7d7a9b259046225854234b6
PsycoCast <gabsiyassine@gmail.com>
2021-05-13 14:39:33 +0100

    navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
+import './Header.css'
 function Header() {
     return(
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand className="col-8 px-4" href="#home">Lets Gtok</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link className="mx-1" href="#home">About</Nav.Link>
-                    <Nav.Link className="mx-1" href="#link">Blogs</Nav.Link>
-                    <Nav.Link className="mx-1" href="#link">Careers</Nav.Link>
-                    <Nav.Link className="mx-1"href="#link">Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <header>
+            <Navbar bg="light" expand="lg" className="sticky-top">
+                <Navbar.Brand className="col-6 px-4" href="#home">Lets Gtok</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link className="mx-3" href="#home">About</Nav.Link>
+                        <Nav.Link className="mx-3" href="#link">Blogs</Nav.Link>
+                        <Nav.Link className="mx-3" href="#link">Careers</Nav.Link>
+                        <Nav.Link className="mx-3" href="#link">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </header>
+
     );
 }
 
