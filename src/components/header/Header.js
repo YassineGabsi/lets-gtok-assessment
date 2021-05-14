80f35444b465129acf04ed34ca30c37888adf2c6
PsycoCast <gabsiyassine@gmail.com>
2021-05-14 03:06:06 +0100

    improve navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import './Header.css'
-function Header() {
-    return(
-            <Navbar bg="light" expand="lg" className="sticky-top">
-                <Navbar.Brand className="col-6 px-4" href="#home">Lets Gtok</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link className="mx-3" href="#home">About</Nav.Link>
-                        <Nav.Link className="mx-3" href="#link">Blogs</Nav.Link>
-                        <Nav.Link className="mx-3" href="#link">Careers</Nav.Link>
-                        <Nav.Link className="mx-3" href="#link">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
 
+function Header() {
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener("scroll", getOffsetWindow);
+    });
+
+    function getOffsetWindow() {
+        setOffset(window.pageYOffset)
+    }
+
+    return (
+        <Navbar  expand="lg" className={offset > 200 ? 'sticky-top' : 'sticky-top sticky-after'}>
+            <Navbar.Brand className="col-6 px-4" href="#home">Lets Gtok</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link className="mx-3" href="#home">About</Nav.Link>
+                    <Nav.Link className="mx-3" href="#link">Blogs</Nav.Link>
+                    <Nav.Link className="mx-3" href="#link">Careers</Nav.Link>
+                    <Nav.Link className="mx-3" href="#link">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
